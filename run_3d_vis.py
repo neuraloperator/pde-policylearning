f8b89e723f8a85c9fda35119ff1a62dab36d47b3
zelin.zhao <sjtuytc@gmail.com>
2023-10-10 15:51:03 +0800

    feat: add 3d visualization

---
+++ run_3d_vis.py
@@ -0,0 +1,63 @@
+import numpy as np
+from mayavi import mlab
+import scipy.io
+
+# Load the MATLAB file
+step = '4001'
+# step =  '1'
+mat_data = scipy.io.loadmat(f'data/channel180_minchan_{step}.mat')
+
+# Extract U, V, and W arrays
+U = mat_data['U']
+V = mat_data['V']
+W = mat_data['W']
+
+# Convert U, V, and W to NumPy arrays
+U = np.array(U)
+V = np.array(V)
+W = np.array(W)
+
+# Filter y direction
+filter_num = 15
+U = U[:, :filter_num, :]
+V = V[:, :filter_num, :]
+W = W[:, :filter_num, :]
+
+# Calculate the velocity gradients
+dU_dx, dU_dy, dU_dz = np.gradient(U, axis=(0, 1, 2))
+dV_dx, dV_dy, dV_dz = np.gradient(V, axis=(0, 1, 2))
+dW_dx, dW_dy, dW_dz = np.gradient(W, axis=(0, 1, 2))
+
+# Assemble the velocity gradient tensor
+gradient_tensor = np.stack((dU_dx, dU_dy, dU_dz, dV_dx, dV_dy, dV_dz, dW_dx, dW_dy, dW_dz), axis=-1)
+gradient_tensor = gradient_tensor.reshape(dU_dx.shape + (3, 3))
+
+# Calculate the discriminant following Topology of fine-scale motions in turbulent channel flow
+trace_gradient_tensor = np.trace(gradient_tensor, axis1=-2, axis2=-1)  # This will give you a 3D array of shape (Nx, Ny, Nz)
+determinant_gradient_tensor = np.linalg.det(gradient_tensor)  # This will give you a 3D array of shape (Nx, Ny, Nz)
+
+reshaped_gradient_tensor = gradient_tensor.reshape(gradient_tensor.shape[:-2] + (3, 3))
+elementwise_product = np.einsum('...ij,...jk->...ik', reshaped_gradient_tensor, reshaped_gradient_tensor)
+trace_elementwise_product = np.trace(elementwise_product, axis1=-2, axis2=-1)  # This will give you a 3D array of shape (Nx, Ny, Nz)
+
+R_value = - trace_gradient_tensor
+Q_value = 0.5 * (trace_gradient_tensor ** 2 - trace_elementwise_product)
+discriminant = 27/4*R_value**2 + Q_value ** 3
+
+
+# Create an isosurface
+mlab.figure(step, bgcolor=(1, 1, 1), fgcolor=(0.,0.,0.), size=(800, 600))
+print(np.unravel_index(np.argmax(discriminant), discriminant.shape))
+mlab.contour3d(discriminant, contours=[5e-5], opacity=0.2, colormap='viridis')
+# mlab.contour3d(discriminant, contours=[0.0], opacity=1.0, colormap='jet')
+# mlab.contour3d(discriminant, contours=[0.0], opacity=1.0, colormap='hot')
+
+# Add axes
+mlab.axes(color=(0, 0, 0), line_width=1.5)
+mlab.xlabel('x')
+mlab.ylabel('y')
+mlab.zlabel('z')
+mlab.view(azimuth=-90, elevation=-150, distance=80)
+
+# Show the plot
+mlab.show()
